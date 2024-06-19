9e6885c9440e64e7a99526fa38dd4e7fe41fc08d
Gigi Dolmazashvili <117160012+Gvamiko@users.noreply.github.com>
2024-06-19 17:06:52 +0400

    Add files via upload

---
+++ src/views/MainView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <NavbarView/>
+ 
+    <div class="container">
+        <div class="row mt-5 borde-manual">
+          <div class="col-md-6 left original-transform position-relative">
+            <div class="block-green position-absolute bottom-0 start-0 z-2"></div>
+            <div class="main position-absolute">
+              <h1>Gigi Dolmazashvili</h1>
+            </div>
+          </div>
+          <div class=" col-md-6 right original-transform position-relative">
+            <div class="main position-absolute">
+              <h1>Gigi Dolmazashvili</h1>
+            </div>
+            <div class="block-red position-absolute top-0 end-0 z-2"></div>
+          </div>
+        </div>
+        
+    </div>
+ 
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarView from '@/components/NavbarView.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavbarView,
+  }
+}
+</script>
