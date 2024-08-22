65ca789a80f746f7c80ae5230053634355fe4c91
Gigi Dolmazashvili <117160012+Gvamiko@users.noreply.github.com>
2024-08-22 23:53:35 +0400

    acord

--- src/components/ServiceView.vue
+++ src/components/ServiceView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="container-fluid ">
+        <div class="container ">
             <div class="row mt-5 matg-t bgr m-0">
                 <div class="col-md-12">
                     <div class="row">
@@ -8,109 +8,216 @@
                             <h4 class="text-center headers">პროდუქტები</h4>
                         </div>
                     </div>
-                    <div class="row justify-content-center">
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <h6></h6>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        კომპიუტერული ტექნიკა
+                                    </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <div class="row justify-content-between">
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <h6></h6>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <h6></h6>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv focus-ring">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-between">
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <h6></h6>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        კომპიუტერული ტექნიკა
+                                    </button>
+                                    </h2>
+                                    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <div class="row justify-content-between">
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-between">
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        კომპიუტერული ტექნიკა
+                                    </button>
+                                    </h2>
+                                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <div class="row justify-content-between">
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-between">
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2 col-2">
+                                                <div class="card card-serv">
+                                                    <h6>კომპიუტერული ტექნიკა</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
                     </div>
-                    <div class="row justify-content-center">
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-2">
-                            <div class="card card-serv">
-                                <h6>კომპიუტერული ტექნიკა</h6>
-                            </div>
-                        </div>
-                    </div>
+                    
+                    
                 </div>
             </div>
         </div>
