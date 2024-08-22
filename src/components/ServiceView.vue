33f2f25109ea0d0feb65d3d4c654c0245063fdba
Gvamiko <gigi.dolmazashvili@gmail.com>
2024-08-22 15:14:50 +0400

    .

--- src/components/ServiceView.vue
+++ src/components/ServiceView.vue
@@ -1,42 +1,121 @@
 <template>
     <div>
-        <div class="container">
-            <div class="row mt-5 matg-t bgr">
+        <div class="container-fluid ">
+            <div class="row mt-5 matg-t bgr m-0">
                 <div class="col-md-12">
                     <div class="row">
-                <div class="col-md-12">
-                    <h4 class="text-center headers">სერვისები</h4>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-6">
-                    <div class="card card-serv">
-                        <h6>კომპიუტერული ტექნიკა</h6>
+                        <div class="col-md-12">
+                            <h4 class="text-center headers">პროდუქტები</h4>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-6">
-                    <div class="card card-serv">
-                        <h6>კომპიუტერული ტექნიკა</h6>
+                    <div class="row justify-content-center">
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <h6></h6>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h6></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-6">
-                    <div class="card card-serv">
-                        <h6>კომპიუტერული ტექნიკა</h6>
+                    <div class="row justify-content-center">
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-6">
-                    <div class="card card-serv">
-                        <h6>კომპიუტერული ტექნიკა</h6>
+                    <div class="row justify-content-center">
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-2">
+                            <div class="card card-serv">
+                                <h6>კომპიუტერული ტექნიკა</h6>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
                 </div>
             </div>
         </div>
     </div>
-  </template>
+</template>
 
 
 <script>
