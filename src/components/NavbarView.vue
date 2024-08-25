3764c2c52fd1a441511ce3721faf083709200077
Gigi Dolmazashvili <117160012+Gvamiko@users.noreply.github.com>
2024-08-25 21:37:46 +0400

    .

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,31 +1,31 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-custom" id="navbar-example2">
-        <div class="container">
-            <a href="/" class="navbar-brand"><span>B</span>yte<span>C</span>raft</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav ms-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="./#scrollspyHeading1">მთავარი</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/#scrollspyHeading2">პარტნიორები</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/#scrollspyHeading3">ჩვენს შესახებ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/#scrollspyHeading4">კონტაქტი</a>
-                </li>
-            </ul>
-                <RouterLink to="/login">
-                    <button class="btn btn-manual-nav" type="submit">ავტორიზაცია</button>
-                </RouterLink>
+            <div class="container">
+                <a href="/" class="navbar-brand"><span>B</span>yte<span>C</span>raft</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./#scrollspyHeading1">მთავარი</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/#scrollspyHeading2">პარტნიორები</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/#scrollspyHeading3">ჩვენს შესახებ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/#scrollspyHeading4">კონტაქტი</a>
+                    </li>
+                </ul>
+                    <RouterLink to="/login">
+                        <button class="btn btn-manual-nav" type="submit">ავტორიზაცია</button>
+                    </RouterLink>
+                </div>
             </div>
-        </div>
         </nav>
         
     </div>
